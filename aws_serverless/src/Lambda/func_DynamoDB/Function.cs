c363dc6ddca1b5cbd2adda9d3e0eeaca83cc2708
serkanbingol <serkanbingol@outlook.com>
2020-05-17 21:26:48 +0300

    Application Finished

--- aws_serverless/src/Lambda/func_DynamoDB/Function.cs
+++ aws_serverless/src/Lambda/func_DynamoDB/Function.cs
@@ -1,16 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Amazon;
 using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
 using Amazon.S3;
 using Amazon.S3.Model;
+using CsvHelper;
+using func_DynamoDB.model;
 using Microsoft.VisualBasic.FileIO;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -27,7 +31,8 @@ namespace func_DynamoDB {
         /// <returns></returns>
         public async Task<string> FunctionHandler (S3Event evnt, ILambdaContext context) {
             RegionEndpoint bucketRegion = RegionEndpoint.EUWest1;
-            var client = new AmazonS3Client (bucketRegion);
+            var s3Client = new AmazonS3Client (bucketRegion);
+            var dynamoClient = new AmazonDynamoDBClient ();
             var s3Event = evnt.Records[0].S3;
 
             context.Logger.LogLine ($"Event Received by Lambda Function from {s3Event.Bucket.Name}");
@@ -38,73 +43,37 @@ namespace func_DynamoDB {
                 Key = s3Event.Object.Key,
 
             };
-            GetObjectResponse response = await client.GetObjectAsync (request);
+            GetObjectResponse response = await s3Client.GetObjectAsync (request);
 
-            response.WriteResponseStreamToFileAsync ("/tmp/inventory.txt", true, new System.Threading.CancellationToken { }).Wait ();
+            response.WriteResponseStreamToFileAsync ("/tmp/" + s3Event.Object.Key + ".txt", true, new System.Threading.CancellationToken { }).Wait ();
+
+            #region CsvHelper Region
             try {
-                using (TextFieldParser csvReader = new TextFieldParser ("/tmp/inventory.txt")) {
-                    csvReader.SetDelimiters (new string[] { "," });
-                    csvReader.HasFieldsEnclosedInQuotes = true;
+                Table inventoryCatalog = Table.LoadTable (dynamoClient, "serverless-inventory-app-dynamodb-table");
+                List<StockDetails> result;
 
-                    string[] colFields;    
-                    colFields = csvReader.ReadFields ();
-                    foreach (string column in colFields) {
-                        context.Logger.LogLine (column);
-                    }
-                    while (!csvReader.EndOfData) {
-                        colFields = csvReader.ReadFields ();
-                        foreach (string column in colFields) {
-                            context.Logger.LogLine (column);
-                        }
+                using (var fileReader = new StreamReader ("/tmp/" + s3Event.Object.Key + ".txt")) {
+                    var csv = new CsvReader (fileReader, CultureInfo.InvariantCulture);
+                    csv.Configuration.HasHeaderRecord = false;
+                    csv.Read ();
+                    result = csv.GetRecords<StockDetails> ().ToList ();
 
+                    foreach (var stock in result) {
+                        var newStock = new Document ();
+                        newStock["Store"] = stock.store;
+                        newStock["Item"] = stock.item;
+                        newStock["Count"] = stock.count;
+                        await inventoryCatalog.PutItemAsync (newStock);
                     }
 
                 }
+                File.Delete ("/tmp/" + s3Event.Object.Key + ".txt");
+                return "Sucess:Parsing CSV and Insert DynamoDB Table";
             } catch (Exception ex) {
                 Console.WriteLine (ex.Message);
-                return "Error:Parsing CSV";
+                return "Error:Parsing CSV and Insert DynamoDB";
             }
-
-            return "TEST COMPLETED";
-
-            #region Stream s3 Object
-            // string responseBody = "";
-            // try {
-            //     GetObjectRequest request = new GetObjectRequest {
-            //         BucketName = s3Event.Bucket.Name,
-            //         Key = s3Event.Object.Key,
-
-            //     };
-            //     using (GetObjectResponse response = await client.GetObjectAsync (request))
-            //     using (Stream responseStream = response.ResponseStream)
-            //     using (StreamReader reader = new StreamReader (responseStream)) {
-            //         // string title = response.Metadata["x-amz-meta-title"]; // Assume you have "title" as medata added to the object.
-            //         string contentType = response.Headers["Content-Type"];
-            //         // context.Logger.LogLine ($"Object metadata, Title: {title}");
-            //         context.Logger.LogLine ($"Content type: {contentType}");
-
-            //         responseBody = reader.ReadToEnd (); // Now you process the response body.
-            //         context.Logger.LogLine (responseBody);
-            //         context.Logger.LogLine ("----------------------");
-
-            //         var array = responseBody.ToString ().Split (',');
-            //         foreach (string column in array) {
-            //             context.Logger.LogLine (column);
-            //         }
-            //         context.Logger.LogLine ("end");
-            //         return "OK";
-
-            //     }
-
-            // } catch (AmazonS3Exception e) {
-            //     context.Logger.LogLine ($"Error encountered ***. Message:'{e.Message}' when writing an object");
-            //     return "ERROR";
-            // } catch (Exception e) {
-            //     context.Logger.LogLine ($"Unknown encountered on server. Message:'{e.Message}' when writing an object");
-            //     return "ERROR";
-            // }
             #endregion
-
         }
 
     }
--- aws_serverless/src/Lambda/func_SNS/Function.cs
+++ aws_serverless/src/Lambda/func_SNS/Function.cs
@@ -2,7 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
 using Amazon.Lambda.Core;
+using Amazon.Lambda.DynamoDBEvents;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly : LambdaSerializer (typeof (Amazon.Lambda.Serialization.SystemTextJson.LambdaJsonSerializer))]
@@ -13,11 +17,44 @@ namespace func_SNS {
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
         /// </summary>
-        /// <param name="input"></param>
+        /// <param name="evnt"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler (string input, ILambdaContext context) {
-            return input?.ToUpper ();
+        public async Task<string> FunctionHandler (DynamoDBEvent dynamoEvent, ILambdaContext context) {
+
+            var client = new AmazonSimpleNotificationServiceClient ();
+
+            context.Logger.LogLine ($"Event Received by Lambda Function from {dynamoEvent}");
+            context.Logger.LogLine ($"Beginning to process {dynamoEvent.Records.Count} records...");
+
+            foreach (var record in dynamoEvent.Records) {
+
+                var dbRecord = record.Dynamodb.NewImage;
+                if (dbRecord != null) {
+                    var itemStore = dbRecord["Store"].S;
+                    var itemName = dbRecord["Item"].S;
+                    var itemStock = Convert.ToInt32 (dbRecord["Count"].N);
+                    var message = itemStore + " is out of stock of " + itemName;
+
+                    context.Logger.LogLine ($"Records {itemStore} - {itemName} - {itemStock.ToString()}");
+
+                    if (itemStock == 0) {
+                        var allTopics = await client.ListTopicsAsync ();
+                        var NoStockTopic = allTopics.Topics.FirstOrDefault (x => x.TopicArn.Contains ("NoStock")).TopicArn;
+                        var request = new PublishRequest {
+                            TopicArn = NoStockTopic,
+                            Message = message,
+                            Subject = "Inventory Alert!"
+                        };
+
+                        client.PublishAsync (request).Wait ();
+
+                    }
+                }
+
+            }
+            return "OK";
         }
+
     }
 }
